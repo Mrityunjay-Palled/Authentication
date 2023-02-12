e13e5a9b2e5701282bdc42166c04dd0920d18dcf
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-02-12 13:12:11 +0530

    user authentication with bcrypt

--- server.js
+++ server.js
@@ -1,23 +1,44 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+app.use(express.json());
 
-const posts = [
-  {
-    username: "Naruto",
-    title: "Post 1",
-  },
-  {
-    username: "Levi",
-    title: "Post 2",
-  },
-];
+const users = [];
 
-app.use(express.json())
-
-app.get("/posts", (req, res) => {
-    res.json(posts)
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
+app.post("/users", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(hashedPassword);
+    const user = {
+      name: req.body.name,
+      password: hashedPassword,
+    };
 
+    users.push(user);
+    res.status(201).send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
 
-app.listen(3000, () => console.log("server started"));
+app.post('/users/login',async (req,res)=>{
+   const user=users.find(user=>user.name===req.body.name)
+   if(user===null){
+    return res.status(400).send('Cannot Find User')
+   }
+   try{
+    if(await bcrypt.compare(req.body.password,user.password)){
+        res.send('Successfully Logged')
+    }else{
+        res.send('Failed To Login')
+    }
+   }catch(err){
+    res.status(500).send();
+   }
+})
+
+app.listen(3000);
