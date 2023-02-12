c12db2bfc6c04360c0c4600f24e24ff3a0c8f105
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-02-12 17:15:54 +0530

    basic jwt auth

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
+const jwt=require('jsonwebtoken')
+
+app.use(express.json())
 
 const posts = [
   {
@@ -14,10 +18,28 @@ const posts = [
 
 app.use(express.json())
 
-app.get("/posts", (req, res) => {
-    res.json(posts)
+app.get("/posts",authenticateToken,(req, res) => {
+    res.json(posts.filter(post=>post.username===req.user.name))
 });
 
+app.post('/login',(req,res)=>{
+ const username=req.body.username
+ const user={name:username}
+ const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN)
+ res.json({accessToken:accessToken})
+})
+
+function authenticateToken(req,res,next){
+   const authHeader=req.headers['authorization']
+   const token=authHeader && authHeader.split(' ')[1]
+   if(token===null) return res.sendStatus(401)
+
+   jwt.verify(token,process.env.ACCESS_TOKEN,(err,user)=>{
+    if(err) return res.sendStatus(403)
+    req.user=user
+    next()
+   })
+}
 
 
-app.listen(3000, () => console.log("server started"));
+app.listen(3000);
